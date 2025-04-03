4f1b2d648c74c302ab65acf4d3c60abd1c7c46bd
karnigen <karnigen@gmail.com>
2025-04-03 23:19:34 +0200

    restructure files

---
+++ src/uvr/uvr.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: #!/usr/bin/env uvr")
+        sys.exit(1)
+
+    script = sys.argv[1]
+    script = os.path.realpath(script)
+    scriptDir = os.path.dirname(script)
+
+    os.execlp('uv', 'uv', 'run', '--project', scriptDir, script, *sys.argv[2:])
+
+if __name__ == "__main__":
+    main()
