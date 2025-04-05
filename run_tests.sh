0ea99841b9cae53f752da87c78eb83ea2fe24e73
karnigen <karnigen@gmail.com>
2025-04-05 13:51:54 +0200

    added coverage

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-uvr pytest
-# uvr pytest --cov-report term-missing --cov-report html --cov-report xml --cov=uvr
+# uvr pytest
+#uvr pytest --cov-report term-missing --cov-report html --cov-report xml --cov=uvr
+
+uvr pytest --cov=uvr --cov-report html:coverage --cov-report xml
