36f964f48c01f2cba95d79a7c2de85ede091a051
karnigen <karnigen@gmail.com>
2025-06-28 11:39:46 +0200

    debug install scrippts

--- uv_scripts/run_tests.sh
+++ uv_scripts/run_tests.sh
@@ -3,4 +3,6 @@
 # uvr pytest
 #uvr pytest --cov-report term-missing --cov-report html --cov-report xml --cov=uvr
 
-uvr pytest --cov=uvr --cov-report html:coverage --cov-report xml --cov-report term
+cd ..
+
+uv run pytest --cov=uvr --cov-report html:coverage --cov-report xml --cov-report term
